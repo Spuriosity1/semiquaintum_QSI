297972dbdb75f3fda6b63f68ce7d6c7240097e65
Spuriosity1 <alaric.sanders@gmail.com>
2026-05-01 07:49:52 +0200

    get rid of emplace_back calls that annoy icpx

--- percol/src/monte_carlo.cpp
+++ percol/src/monte_carlo.cpp
@@ -233,10 +233,10 @@ int try_flip_ring(Plaq* p, MCSettings& mc) {
     for (QClusterMF* qc : touched) {
         for (const auto& b : qc->mf_bonds) {
             if (!touched.count(b.other)) {
-                quantum_quantum_bonds.emplace_back(qc, b.my_site, b.other, b.other_site);
+                quantum_quantum_bonds.push_back({qc, b.my_site, b.other, b.other_site});
             } else if (b.other < qc){
                 // pointer comparison trick avoids double counting
-                quantum_quantum_bonds.emplace_back(qc, b.my_site, b.other, b.other_site);
+                quantum_quantum_bonds.push_back({qc, b.my_site, b.other, b.other_site});
             }
         }
     }
@@ -388,10 +388,10 @@ int try_flip_worm(MCSettings& mc, Spin* root) {
     for (QClusterMF* qc : touched) {
         for (const auto& b : qc->mf_bonds) {
             if (!touched.count(b.other)) {
-                quantum_quantum_bonds.emplace_back(qc, b.my_site, b.other, b.other_site);
+                quantum_quantum_bonds.push_back({qc, b.my_site, b.other, b.other_site});
             } else if (b.other < qc){
                 // pointer comparison trick avoids double counting
-                quantum_quantum_bonds.emplace_back(qc, b.my_site, b.other, b.other_site);
+                quantum_quantum_bonds.push_back({qc, b.my_site, b.other, b.other_site});
             }
         }
     }
